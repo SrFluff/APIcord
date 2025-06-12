f83388f871df6a12b174c9de6fb4854091b8d76a
franco <fluffins007@outlook.com>
2025-06-12 18:27:42 +0100

    Guh

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return "Fuhh you"
+
+@app.get("/image")
+def root():
+    return FileResponse("image.png",media_type="image/png")
